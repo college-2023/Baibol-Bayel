c0e834804a26a433aa4ec39e2b99f543c2327fa2
college-2023 <123530301+college-2023@users.noreply.github.com>
2023-03-13 16:45:50 +0600

    Delete Adilet/devsearch directory

--- Adilet/devsearch/static/js/placeholder.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.input-form input[name="username"]').placeholder = 'username'
-document.querySelector('.input-form input[name="password1"]').placeholder = 'password'
-document.querySelector('.input-form input[name="password2"]').placeholder = 'repeat password'  
