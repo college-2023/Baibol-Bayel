804620f2d8f0f2735cea6a2686fa529881f5b1b1
college-2023 <123530301+college-2023@users.noreply.github.com>
2023-03-13 17:03:15 +0600

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w2@4fvi69+$!568sea!=je$lqr70l!da0r-3bsczm4wye$+c@v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres51',
+        'USER': 'postgres51',
+        'PASSWORD': 'postgres51',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
@@ -118,12 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
